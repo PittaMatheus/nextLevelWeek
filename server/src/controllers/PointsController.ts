b46b23844721f93b53afbc27ecc1928149a10c8a
Matheus Pitta <matheus.pitta12@gmail.com>
2020-06-12 23:58:13 -0300

    fix route

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -4,6 +4,33 @@ import Knex from '../database/connection'
 
 class PointsController {
 
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await Knex('points')
+      .join('point_items', 'points_id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+
+      const serializedPoints = points.map(point => {
+        return {
+          ...point,
+          image_url: `http://192.168.0.156:3333/uploads/${point.image}`,
+        };
+      });
+
+    return response.json(serializedPoints)
+
+  }
+
   async show(request: Request, response: Response) {
     const { id } = request.params;
 
@@ -70,25 +97,7 @@ class PointsController {
     });
   }
 
-  async index(request: Request, response: Response) {
-    const { city, uf, items } = request.query;
-
-    const parsedItems = String(items)
-      .split(',')
-      .map(item => Number(item.trim()));
-
-    const points = await Knex('points')
-      .join('point_items', 'points_id', '=', 'point_items.point_id')
-      .whereIn('point_items.item_id', parsedItems)
-      .where('city', String(city))
-      .where('uf', String(uf))
-      .distinct()
-      .select('points.*');
-
-
-    return response.json(points)
-
-  }
+  
 
 
 
